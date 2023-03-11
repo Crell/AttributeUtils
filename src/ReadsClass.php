1a1fd542ff5ae93f281baaf321844d4874a9ccea
Larry Garfield <larry@garfieldtech.com>
2023-03-11 10:26:04 -0600

    Improve documentation.

--- src/ReadsClass.php
+++ src/ReadsClass.php
@@ -4,6 +4,18 @@ declare(strict_types=1);
 
 namespace Crell\AttributeUtils;
 
+/**
+ * Marks a component-targeting attribute as wanting the class attribute passed to it.
+ *
+ * If a component-targeting attribute implements this interface, then after it
+ * is instantiated and all other opt-in behaviors have run, the corresponding class
+ * attribute will be passed to this method.  The attribute may then extract whatever
+ * information it desires and save it to properties however it likes.
+ *
+ * Note that the attribute SHOULD NOT save the class itself in most cases.  It is
+ * better to materialize the necessary information onto the attribute, so that it
+ * may be accessed more quickly in the future.  Also, circular dependencies get messy.
+ */
 interface ReadsClass
 {
     public function fromClassAttribute(object $class): void;
